e03040b62101a034c25202a2b8936473155d6b36
Arthur Wang <arthurwzh@sina.cn>
2020-08-27 17:11:00 -0400

    Initial Commit

---
+++ HTTPClient/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HTTPClient
+//
+//  Created by 王子豪 on 8/27/20.
+//  Copyright © 2020 王子豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
